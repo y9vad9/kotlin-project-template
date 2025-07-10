0579c12ec3567845aad6c42d65313263743bd09a
Vadym Yaroshchuk <y9vad9@gmail.com>
2025-07-10 02:13:49 +0200

    feat: add default conventions, workflows, and documentation
    - Added default Gradle conventions: kover, detekt, tests
    - Introduced shared convention plugin IDs via buildSrc
    - Added default  optimizations
    - Included GitHub workflows for tests, coverage, detekt, and code quality
    - Updated README with usage and configuration documentation

--- build-conventions/build.gradle.kts
+++ build-conventions/build.gradle.kts
@@ -13,5 +13,8 @@ kotlin {
 }
 
 dependencies {
-    api(libs.kotlin.plugin)
+    api(libs.pluginClasspath.kotlin)
+    api(libs.pluginClasspath.kover)
+    api(libs.pluginClasspath.detekt)
+    api(files((libs).javaClass.superclass.protectionDomain.codeSource.location))
 }
